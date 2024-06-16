224312e72f4258c4c99877837dae7bd876b03042
Ukkey <develop@ukihome.com>
2024-06-16 23:14:55 +0900

    chore: Update Firebase initialization in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,16 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:ouchi_stocker/data/firebase_options.dart';
 import 'package:ouchi_stocker/view/app.dart';
 
-void main() => runApp(const App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const App());
+}
