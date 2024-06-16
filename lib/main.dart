05e0266c98ffb008300c47a8767d3c31bc12f774
Ukkey <develop@ukihome.com>
2024-06-16 23:40:58 +0900

    Move firebase_options.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ouchi_stocker/data/firebase_options.dart';
+import 'package:ouchi_stocker/constants/firebase_options.dart';
 import 'package:ouchi_stocker/util/loggers/provider_logger.dart';
 import 'package:ouchi_stocker/view/app.dart';
 
